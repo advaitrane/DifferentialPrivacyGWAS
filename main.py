69065ece226d79eb5fe4bb6437a7ca333fb6f54b
advaitrane <advaitr11@gmail.com>
2022-11-18 12:50:04 -0800

    neighbour distance initial commit

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import util
 from util import read_bed_for_snp, read_phenotype
 from statistics.standard_stats import get_genotype_dist, chi_squared_test, allelic_test
 from statistics.dp_stats import top_SNPs_laplace, top_SNPs_exponential, chi_squared_test_sensitivity, allelic_test_sensitivity
+from statistics.dp_stats import get_neighbour_distance
 
 DATA_DIR = "../data/hapmap_JPT_CHB_r23a_filtered"
 file_name = "hapmap_JPT_CHB_r23a_filtered"
 phenotype_file_name = "simu.pheno"
 
 def main():
-	snp_idx = 2463
+	snp_idx = 219
 	genotype_list_0 = read_bed_for_snp(DATA_DIR, file_name, snp_idx)
 
 	phenotype_list = read_phenotype(DATA_DIR, phenotype_file_name)
@@ -17,36 +18,39 @@ def main():
 	genotype_dist = get_genotype_dist(genotype_list_0, phenotype_list)
 	print(f"Genotype distribution for SNP {snp_idx} - {genotype_dist}")
 	
-	chi_squared_value = chi_squared_test(genotype_dist)
-	print(f"Chi squared value for SNP {snp_idx} - {chi_squared_value}")
+	# chi_squared_value = chi_squared_test(genotype_dist)
+	# print(f"Chi squared value for SNP {snp_idx} - {chi_squared_value}")
 	allelic_test_value = allelic_test(genotype_dist)
 	print(f"Allelic test value for SNP {snp_idx} - {allelic_test_value}")
 
-	M_SNPS_laplace = top_SNPs_laplace(
-		DATA_DIR, 
-		file_name, 
-		phenotype_file_name,
-		chi_squared_test,
-		chi_squared_test_sensitivity,
-		10,
-		1000,
-		1000
-		)
-	print("Top SNPs given by the laplace algorithm")
-	print(M_SNPS_laplace)
+	# M_SNPS_laplace = top_SNPs_laplace(
+	# 	DATA_DIR, 
+	# 	file_name, 
+	# 	phenotype_file_name,
+	# 	chi_squared_test,
+	# 	chi_squared_test_sensitivity,
+	# 	10,
+	# 	1000,
+	# 	1000
+	# 	)
+	# print("Top SNPs given by the laplace algorithm")
+	# print(M_SNPS_laplace)
 
-	M_SNPS_exp = top_SNPs_exponential(
-		DATA_DIR, 
-		file_name, 
-		phenotype_file_name,
-		chi_squared_test,
-		chi_squared_test_sensitivity,
-		10,
-		1000,
-		1000
-		)
-	print("Top SNPs given by the exponential algorithm")
-	print(M_SNPS_exp)
+	# M_SNPS_exp = top_SNPs_exponential(
+	# 	DATA_DIR, 
+	# 	file_name, 
+	# 	phenotype_file_name,
+	# 	chi_squared_test,
+	# 	chi_squared_test_sensitivity,
+	# 	10,
+	# 	1000,
+	# 	1000
+	# 	)
+	# print("Top SNPs given by the exponential algorithm")
+	# print(M_SNPS_exp)
+
+	d = get_neighbour_distance(genotype_dist, 10, allelic_test_value)
+	print(f'Neighbour distance = {d}')
 
 if __name__ == '__main__':
 	main()
--- statistics/dp_stats.py
+++ statistics/dp_stats.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import heapq
 from util import read_bed_for_snp, read_phenotype
 from statistics.standard_stats import get_genotype_dist
@@ -148,6 +149,305 @@ def top_SNPs_exponential(
 		w_SNPs /= w_sum
 
 	return M_SNPs
+
+def get_neighbour_distance(genotype_dist, w, Y):
+	# def point_on_C(genotype_dist, x, y, w):
+	# 	lhs = 2*genotype_dist['N']*(
+	# 		x*genotype_dist['S'] - y*genotype_dist['R']
+	# 		)**2
+	# 	rhs = genotype_dist['R']*genotype_dist['S']*w*(x + y)*(
+	# 		2*genotype_dist['N'] - x - y
+	# 		)
+	# 	return lhs == rhs
+
+	N = genotype_dist['N']
+	R = genotype_dist['R']
+	S = genotype_dist['S']
+	r0 = genotype_dist['r0']
+	r1 = genotype_dist['r1']
+	r2 = genotype_dist['r2']
+	s0 = genotype_dist['s0']
+	s1 = genotype_dist['s1']
+	s2 = genotype_dist['s2']
+
+	def get_P():
+		P = set()
+		m_vals = [1, 2, 0.5]
+
+		for m in m_vals:
+			alpha = 2*N*R*S*m - R*S*w*m - 2*N*S*S - R*S*w
+			beta = N*R*S*w*(1+m)
+			gamma = 2*N*R*R*m + R*S*w*m + R*S*w - 2*N*R*S
+			alpha /= gamma
+			beta /= gamma
+
+			A = 2*N*(S-alpha*R)**2 + R*S*w*(1+alpha)**2
+			B = 4*N*beta*R*(alpha*R-S) - 2*R*S*w*(N - beta)*(1 + alpha)
+			C = 2*N*beta*beta*R*R - R*S*w*beta*(2*N - beta)
+
+			disc = B*B - 4*A*C
+			if disc >= 0:
+				x = (-B + np.sqrt(disc))/(2*A)
+				y = alpha*x + beta
+				if not (np.isnan(x) or math.isinf(x)):
+					if x>=0 and x<=2*R and y>=0 and y<=2*S:
+						P.add((x, y))
+
+				x = (-B - np.sqrt(disc))/(2*A)
+				y = alpha*x + beta
+				if not (np.isnan(x) or math.isinf(x)):
+					if x>=0 and x<=2*R and y>=0 and y<=2*S:
+						P.add((x, y))
+		return P
+
+	def get_y_for_x(x):
+		A = 2*N*R*R + R*S*w
+		B = 2*R*S*w*x - 2*N*R*S*w - 4*N*R*S*x
+		C = 2*N*S*S*x*x + R*S*w*x*x - 2*N*R*S*w*x
+
+		d = B*B - 4*A*C
+
+		if d >= 0:
+			y1 = (-B + np.sqrt(d)) / (2*A)
+			y2 = (-B - np.sqrt(d)) / (2*A)
+			return y1, y2
+		return None, None
+
+	def get_x_for_y(y):
+		A = 2*N*S*S + R*S*w
+		B = 2*R*S*w*y - 2*N*R*S*w - 4*N*R*S*y
+		C = 2*N*S*S*y*y + R*S*w*y*y - 2*N*R*S*w*y
+
+		d = B*B - 4*A*C
+
+		if d >= 0:
+			x1 = (-B + np.sqrt(d)) / (2*A)
+			x2 = (-B - np.sqrt(d)) / (2*A)
+			return x1, x2
+		return None, None
+
+	def get_Q():
+		Q = set()
+		x_vals = [
+			2*(genotype_dist['r0'] + genotype_dist['r2']) + genotype_dist['r1'],
+			2*genotype_dist['r0'] + genotype_dist['r1'],
+			genotype_dist['r1'],
+			0,
+			2*R
+		]
+		y_vals = [
+			2*(genotype_dist['s0'] + genotype_dist['s2']) + genotype_dist['s1'],
+			2*genotype_dist['s0'] + genotype_dist['s1'],
+			genotype_dist['s1'],
+			0,
+			2*S
+		]
+
+		for x in x_vals:
+			y1, y2 = get_y_for_x(x)
+			if (y1 is not None and y1 >= 0 and y1 <= 2*S and not (np.isnan(y1) or math.isinf(y1))):
+				Q.add((x, y1))
+			if (y2 is not None and y2 >= 0 and y2 <= 2*S and not (np.isnan(y2) or math.isinf(y2))):
+				Q.add((x, y2))
+
+		for y in y_vals:
+			x1, x2 = get_x_for_y(y)
+			if (x1 is not None and x1 >= 0 and x1 <= 2*R and not (np.isnan(x1) or math.isinf(x1))):
+				Q.add((x1, y))
+			if (x2 is not None and x2 >= 0 and x2 <= 2*R and not (np.isnan(x2) or math.isinf(x2))):
+				Q.add((x2, y))
+
+		return Q
+
+	def g1_x(x):
+		if (x <= 2*R - r1 and 
+			x >= 2*r0 + r1
+		):
+			return (x-2*r0-r1)/2
+		elif (x >= r1 and 
+			x <= 2*r0+r1
+		):
+			return (2*r0+r1-x)/2
+		elif (x <= 2*R and x >= 2*R - r1):
+			return r2+x-2*(r0+r2)-r1
+		else:
+			return r0+r1-x
+
+	def g2_y(y):
+		if (y <= 2*S - s1 and 
+			y >= 2*s0 + s1
+		):
+			return (y-2*s0-s1)/2
+		elif (y >= s1 and 
+			y <= 2*s0+s1
+		):
+			return (2*s0+s1-y)/2
+		elif (y <= 2*S and y >= 2*S - s1):
+			return s2+y-2*(s0+s2)-s1
+		else:
+			return s0+s1-y
+
+	def g_xy(x, y):
+		return g1_x(x) + g2_y(y)
+
+	def beta1_x(x):
+		if (r1 == 0 and 
+			(x-2*r0-r1)%2 == 1
+		):
+			return np.ceil(g1_x(x))+1
+		else:
+			return np.ceil(g1_x(x))
+
+	def beta2_y(y):
+		if (s1 == 0 and 
+			(y-2*s0-s1)%2 == 1
+		):
+			return np.ceil(g2_y(y))+1
+		else:
+			return np.ceil(g2_y(y))
+
+	def get_limits_for_Ui(i):
+		if r1>0 and s1>0:
+			if i == 0 or i == 1:
+				return (2*r0+r1, 2*(r0+r2)+r1)
+			if i == 2 or i == 3:
+				return (r1, 2*r0+r1)
+			if i == 4 or i == 5:
+				return (2*(r0+r2)+r1, 2*R)
+			if i == 6 or i == 7:
+				return (0, r1)
+
+	def get_limits_for_Uprimej(j, alpha, beta):
+		if r1>0 and s1>0:
+			if j == 0 or j == 1:
+				l1 = (2*s0+s1-beta)/alpha
+				l2 = (2*(s0+s2)+s1-beta)/alpha
+			if j == 2 or j == 3:
+				l1 = (s1-beta)/alpha
+				l2 = (2*s0+s1-beta)/alpha
+			if j == 4 or j == 5:
+				l1 = (2*(s0+s2)+s1-beta)/alpha
+				l2 = (2*S-beta)/alpha
+			if j == 6 or j == 7:
+				l1 = (-beta)/alpha
+				l2 = (s1-beta)/alpha
+
+			l_min = min(l1, l2)
+			l_max = max(l1, l2)
+			return l_min, l_max
+
+	def get_alpha_beta(i, j, delta):
+		vi_list = [1/2, 1/2 ,-1/2, -1/2, 1, 1, -1, -1]
+		di_list = [-r0-r1/2, -r0-r1/2+1/2, r0+r1/2, r0+r1/2+1/2, r2-2*(r0+r2)-r1, r2-2*(r0+r2)-r1, r0+r1, r0+r1]
+
+		vj_list = [1/2, 1/2 ,-1/2, -1/2, 1, 1, -1, -1]
+		dj_list = [-s0-s1/2, -s0-s1/2+1/2, s0+s1/2, s0+s1/2+1/2, s2-2*(s0+s2)-s1, s2-2*(s0+s2)-s1, s0+s1, s0+s1]
+
+		vi = vi_list[i]
+		di = di_list[i]
+		vj = vj_list[j]
+		dj = dj_list[j]
+
+		alpha = -vi/vj
+		beta = (delta - di - dj)/vj
+
+		return alpha, beta
+
+	def get_limits_i_j(i, j, delta):
+		alpha, beta = get_alpha_beta(i, j, delta)
+
+		l1, l2 = get_limits_for_Ui(i)
+		l3, l4 = get_limits_for_Uprimej(j, alpha, beta)
+		
+		if l2<l3 or l4<l1:
+			return None, None
+
+		if l3>l1:
+			l1 = l3
+		if l4<l2:
+			l2 = l4
+
+		return l1, l2
+
+	def get_limits_Y(alpha, beta):
+		A = 2*N*(S-alpha*R)**2 + R*S*w*(1+alpha)**2
+		B = 4*N*beta*R*(alpha*R-S) - 2*R*S*w*(N - beta)*(1 + alpha)
+		C = 2*N*beta*beta*R*R - R*S*w*beta*(2*N - beta)
+
+		d = B*B - 4*A*C
+		if d >= 0:
+			l1 = (-B - np.sqrt(d))/(2*A)
+			l2 = (-B + np.sqrt(d))/(2*A)
+			return l1, l2
+		return None, None
+
+	def get_r_s(i, j, delta):
+		if r1>0 and s1>0:
+			l1, l2 = get_limits_i_j(i, j, delta)
+			alpha, beta = get_alpha_beta(i, j, delta)
+			l3, l4 = get_limits_Y(alpha, beta)
+
+			if l1 is None or l2 is None or l3 is None or l4 is None:
+				return None, None
+			if l2<l3 or l4<l1:
+				return None, None
+			r = max(l1, l3)
+			s = min(l2, l4)
+			return r, s
+
+	def check_delta(delta):
+		if r1>0 and s1>0:
+			ci_vals = [-2*r0-r1, -2*r0-r1+1, -2*r0-r1, -2*r0-r1+1, 0, 1, 0, 1]
+			cj_vals = [-2*s0-s1, -2*s0-s1+1, -2*s0-s1, -2*s0-s1+1, 0, 1, 0, 1]
+
+			for i in range(8):
+				for j in range(8):
+					alpha, beta = get_alpha_beta(i, j, delta)
+					r, s = get_r_s(i, j, delta)
+
+					if r is None:
+						continue
+					
+					r = np.ceil(r)
+
+					for k in range(5):
+						t = r+k
+						x = t
+						y = alpha*t + beta
+
+						ci = x+ci_vals[i]
+						cj = y+cj_vals[j]
+
+						if ci%2==0 and cj%2==0:
+							return True
+			return False
+		else:
+			print(f"r1 and s1 don't match condition - ({r1}, {s1})")
+
+	P = get_P()
+	Q = get_Q()
+
+	PQ = P.union(Q)
+	g_hat = None
+	for x, y in PQ:
+		g_val = np.ceil(g_xy(x, y))
+		if g_hat is None or g_hat > g_val:
+			g_hat = g_val
+
+	if Y < w:
+		return g_hat
+
+	for i in range(6):
+		delta = g_hat+i
+		if check_delta(delta):
+			return delta
+
+	return None
+
+
+
+
+
 
 
 
